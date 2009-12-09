e204b65183f3572096e253947b575ef57d224978
Art Tevs <art@tevs.eu>
2009-12-09 23:29:38 +0000

    - added avr project for the bordmonitor - should go now to the NetBeans IDE and remove that fucking Eclipse from the disk

---
+++ BordMonitor/main.c
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.c
+ * Author: tevs
+ *
+ * Created on December 10, 2009, 12:24 AM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv)
+{
+
+    return (EXIT_SUCCESS);
+}
+
