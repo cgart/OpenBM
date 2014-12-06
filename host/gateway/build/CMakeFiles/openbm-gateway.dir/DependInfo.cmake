f43669a680f8ec0bec907c53f7de9ed9f03b8ea0
Art Tevs <art.tevs@dacuda.com>
2014-12-06 16:28:26 +0100

    moved gateway and client to the host folder

---
+++ host/gateway/build/CMakeFiles/openbm-gateway.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/tevs/src/OpenBM/gateway/Application.cpp" "/home/tevs/src/OpenBM/gateway/build/CMakeFiles/openbm-gateway.dir/Application.cpp.o"
+  "/home/tevs/src/OpenBM/gateway/ApplicationClient.cpp" "/home/tevs/src/OpenBM/gateway/build/CMakeFiles/openbm-gateway.dir/ApplicationClient.cpp.o"
+  "/home/tevs/src/OpenBM/gateway/IBus.cpp" "/home/tevs/src/OpenBM/gateway/build/CMakeFiles/openbm-gateway.dir/IBus.cpp.o"
+  "/home/tevs/src/OpenBM/gateway/Log.cpp" "/home/tevs/src/OpenBM/gateway/build/CMakeFiles/openbm-gateway.dir/Log.cpp.o"
+  "/home/tevs/src/OpenBM/gateway/main.cpp" "/home/tevs/src/OpenBM/gateway/build/CMakeFiles/openbm-gateway.dir/main.cpp.o"
+  "/home/tevs/src/OpenBM/gateway/mongoose.cpp" "/home/tevs/src/OpenBM/gateway/build/CMakeFiles/openbm-gateway.dir/mongoose.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+set(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ host/gateway/build/CMakeFiles/openbm-gateway.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/openbm-gateway.dir/main.cpp.o"
+  "CMakeFiles/openbm-gateway.dir/ApplicationClient.cpp.o"
+  "CMakeFiles/openbm-gateway.dir/Application.cpp.o"
+  "CMakeFiles/openbm-gateway.dir/IBus.cpp.o"
+  "CMakeFiles/openbm-gateway.dir/Log.cpp.o"
+  "CMakeFiles/openbm-gateway.dir/mongoose.cpp.o"
+  "openbm-gateway.pdb"
+  "openbm-gateway"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/openbm-gateway.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
